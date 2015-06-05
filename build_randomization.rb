95d94e50a6e40940e81fb417cee56dfd32acdd35
Bill Hwang <bill.hwang@elasticsearch.com>
2015-06-05 10:37:31 -0700

    Add 8u40 to window randomization

--- build_randomization.rb
+++ build_randomization.rb
@@ -351,9 +351,9 @@ unless(C[:test])
           #TODO: better logic
           L.debug("Window Mode")
           if(File.directory?('y:\jdk7\7u55'))   #old window system under ec2
-             FixedJDKSelector.new(['y:\jdk7\7u55', 'y:\jdk8\8u11'])
+             FixedJDKSelector.new(['y:\jdk7\7u55', 'y:\jdk8\8u40'])
           else  #new metal window system
-             FixedJDKSelector.new(['c:\PROGRA~1\JAVA\jdk1.8.0_11', 'c:\PROGRA~1\JAVA\jdk1.7.0_55', 'c:\PROGRA~1\Zulu\zulu-8'])
+             FixedJDKSelector.new(['c:\PROGRA~1\JAVA\jdk1.8.0_40', 'c:\PROGRA~1\JAVA\jdk1.7.0_55', 'c:\PROGRA~1\Zulu\zulu-8'])
           end
         else
           #Jenkins sets pwd prior to execution
