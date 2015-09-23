0df653d619befad306cb67f4a8f24b42706599f5
Bill Hwang <bill.hwang@elasticsearch.com>
2015-09-23 12:41:38 -0700

    jjb: ci repo update script refactoring

--- jjb/build-us-00/common/scripts/ci-update.sh
+++ jjb/build-us-00/common/scripts/ci-update.sh
@@ -1,16 +1,23 @@
 #!/bin/bash -x
 # perform atomic update of the userContent
-old_content=$JENKINS_HOME/userContent
+#
+# the key point is use of mv -T which is supposed to do atomic symbolic link update
+#
+old_content=$JENKINS_HOME/userContent/jjb/${GIT_BRANCH}
+jjb_directory=`dirname $old_content`
 new_content=${old_content}.new
-cp -r -f $WORKSPACE $JENKINS_HOME/$BUILD_TAG
-ln -s $JENKINS_HOME/$BUILD_TAG $new_content
+
+mkdir -p $jjb_directory
+cp -r -f $WORKSPACE $jjb_directory/$BUILD_TAG
+ln -s $jjb_directory/$BUILD_TAG $new_content
 if [ -e $old_content ]
 then
-    mv -f -t $new_content $old_content
+    mv -f -T $new_content $old_content
 else
-    ln -s $JENKINS_HOME/$BUILD_TAG $old_content
+    ln -s `readlink $new_content` $old_content
     rm  $new_content
 fi
+
 # clean up old ones
 directory_prefix=${BUILD_TAG%-*}
-find $JENKINS_HOME -type d -maxdepth 1 ! -name $BUILD_TAG -name "${directory_prefix}*" -exec rm -r -f {} \;
+find $jjb_directory -type d -maxdepth 1 ! -name $BUILD_TAG -name "${directory_prefix}*" -exec rm -r -f {} \;
