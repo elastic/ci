19c83aa7193feed7da88c1108e5d7a03f725e3b4
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-05-23 15:26:20 -0700

    Add the new logger format to Gradle params
    Related to #1099

--- build_randomization.rb
+++ build_randomization.rb
@@ -298,6 +298,7 @@ class RandomizedRunner
     end
     if(ENV['JOB_NAME'].include?('es_core_master_windows'))
         gradleparams.push('-Dtests.logger.level=DEBUG')
+        gradleparams.push('-Dtests.es.logger.level=DEBUG')
     else
         gradleparams.push('-Des.logger.level=DEBUG')
     end
